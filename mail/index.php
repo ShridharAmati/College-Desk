d009d2d43166bb1aba40e5731da83029b94513df
Anish-Guduri <71063609+Anish-Guduri@users.noreply.github.com>
2021-09-20 22:08:18 +0530

    Update index.php

--- mail/index.php
+++ mail/index.php
@@ -1,38 +1,35 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
 require './PHPMailer/src/Exception.php';
 require './PHPMailer/src/PHPMailer.php';
 require './PHPMailer/src/SMTP.php';
-
 if(isset($_POST['email']))
 {
-    $mail = new PHPMailer(true);
-    $mail->isSMTP();
-    // $mail->Mailer = 'smtp';
-    // $mail->SMTPDebug = 1;
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = "anishguduri7@gmail.com"
-    $mail->Password = "Narsaiah7";
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port = 465;
-    // $mail->isHTML(true);
-    $mail->setFrom("$_POST['email']","$_POST['name']");
-    $mail->addAddress("anishguduri7@gmail.com","To College Desk");
-    $mail->Subject = "$_POST['subject']";
-    $mail->Body ='<b>name: </b>' .$_POST['name'] .'<br>' .'<b>Email: </b>' .$_POST['email'] .'<br>' .'<b>Message:</b>' .$POST['message'];
-    // $content = "$_POST['message']"
-    // $mail->MsgHTML($content);
-    if($mail->Send()){
-    echo json_encode(array("code"=> 200, "message" = "Mail Sent"));
-    }
-    else{
-        echo json_encode(array("code"=> 500, "message" = "Something went wrong","error" => $mail->ErrorInfo));
-    }
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";  
+$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "anishguduri7@gmail.com";
+$mail->Password   = "jnjnsljllwevfuhc";
+$mail->IsHTML(true);
+$mail->AddAddress("gudurianish@gmail.com", "Anish Guduri");
+$mail->SetFrom("anishguduri7@gmail.com", "Admin");
+$mail->Subject =$_POST['subject'];
+$content='<b>Name: </b>' .$_POST['name'] .'<br><br>' .'<b>Email: </b>' .$_POST['email'] .'<br><br>' .'<b>Subject: </b>' .$_POST['subject'] .'<br><br>' .'<b>Message:</b>' .$_POST['message'];
+$mail->MsgHTML($content); 
+if(!$mail->Send()) {
+  echo "Error while sending Email.";
+  var_dump($mail);
+} else {
+  echo "Email sent successfully";
 }
+} 
 else{
-    header('location: ../');
-}
+     header('location: ../');
+}   
 ?>
